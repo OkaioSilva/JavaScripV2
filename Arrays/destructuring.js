fd1c83b577b72e5c629a5e30548c0266c1e0ca7e
Kaio silva <Silv4Kaio@hotmail.com>
2024-05-16 17:25:30 -0300

    Create destructuring.js

---
+++ Arrays/destructuring.js
@@ -0,0 +1,10 @@
+const arr = [1,2,3]
+
+// destructuring = serve para recuperar variáveis de uma array.
+// serve para arrays e objetos
+
+// sintaxe 
+const [n1] = arr
+console.log(n1)
+// = 1 
+// vai regatar o primeiro valor, pois só passei "n1" como parametro

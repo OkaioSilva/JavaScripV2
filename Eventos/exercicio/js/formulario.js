da47a95e7fd2ddd420491479eb0accee2cd4123e
Kaio silva <Silv4Kaio@hotmail.com>
2024-05-28 16:33:49 -0300

    Update formulario.html and formulario.js

--- Eventos/exercicio/js/formulario.js
+++ Eventos/exercicio/js/formulario.js
@@ -19,9 +19,57 @@
     formCadastro.addEventListener('submit', function(e){
         console.log(txtTitulo.value)
         if (!txtTitulo.value){
-            alert('Preencha todos os campos')
+            showErrorMsg('Preencha todos os campos', function(){
+                
+                txtTitulo.focus()
+            })
             e.preventDefault()
-            txtTitulo.focus()
         }
     })
+    const feedbackMessage = document.getElementById('feedbackMessage')
+    const feedbackMessageCloseBtn = feedbackMessage.getElementsByTagName('button')[0]
+
+    function showErrorMsg(msg, cb){ 
+        //alert(msg)
+        // feedbackMessage.setAttribute('class', 'show')
+        // quando estamos trabalhando com classes, usar isso não é o ideal, pois posso ter mais de uma classe num elemento
+        // quando estiver trabalhando com classes, o ideal é utilizar uma API específica para isso
+        feedbackMessage.classList.add('show')
+        feedbackMessage.getElementsByTagName('p')[0].textContent = msg
+
+        if (cb === 'function'){
+            cb()
+        }
+
+    }
+    feedbackMessageCloseBtn.addEventListener('click', function(){
+        feedbackMessage.classList.remove('show')
+    })
+
+    const txtDescricao = document.getElementById('txtDescricao')
+    const contadorContainer = document.getElementById('contador')
+    const resta = contadorContainer.getElementsByTagName('span')[0]
+    const maxima = txtDescricao.maxLength
+    mostrarNumero(maxima)
+
+    contadorContainer.removeAttribute('style')
+
+    function checkLength(){
+        let nLetrasDigitadas = this.value.length
+        let caracterRestante = parseInt(maxima - nLetrasDigitadas)
+        mostrarNumero(caracterRestante)
+    }
+    function mostrarNumero(n){
+        resta.textContent = n
+    }
+    // evento 'input' é sempre que ocorrer uma alteração no meu input
+    txtDescricao.addEventListener('input', checkLength )
+
+    btn.disabled = true
+    const chkAceito = document.getElementById('chkAceito')
+    
+    function changechk(){
+        btn.disabled = !this.checked
+    }
+    chkAceito.addEventListener('change', changechk)
 })()

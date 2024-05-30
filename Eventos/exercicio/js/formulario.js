229e2ee6d7de2e9f53a777419aede9f103760a05
Kaio silva <Silv4Kaio@hotmail.com>
2024-05-30 15:37:34 -0300

    Update formulario.js

--- Eventos/exercicio/js/formulario.js
+++ Eventos/exercicio/js/formulario.js
@@ -1,15 +1,20 @@
 (function () {
     'use strict'
-
+    // variáveis 
     const txtTitulo = document.getElementById('txtTitulo')
     const btn = document.getElementById('btn')
     const formCadastro = document.querySelector('.formCadastro')
+    const feedbackMessage = document.getElementById('feedbackMessage')
+    const feedbackMessageCloseBtn = feedbackMessage.getElementsByTagName('button')[0]
+    const txtDescricao = document.getElementById('txtDescricao')
+    const contadorContainer = document.getElementById('contador')
+    const resta = contadorContainer.getElementsByTagName('span')[0]
+    const maxima = txtDescricao.maxLength
     //btn.addEventListener('click', function(e){
     //    console.log(txtTitulo.value)
     //    if (!txtTitulo.value){
     //        alert('Preencha todos os campos')
     //        e.preventDefault() // previne o comportamento padrão, nesse caso, o envio do formulário
-
     // dar foco no input
     //        txtTitulo.focus()
     //    }
@@ -26,32 +31,22 @@
             e.preventDefault()
         }
     })
-    const feedbackMessage = document.getElementById('feedbackMessage')
-    const feedbackMessageCloseBtn = feedbackMessage.getElementsByTagName('button')[0]
 
     function showErrorMsg(msg, cb) {
-        //alert(msg)
         // feedbackMessage.setAttribute('class', 'show')
-        // quando estamos trabalhando com classes, usar isso não é o ideal, pois posso ter mais de uma classe num elemento
-        // quando estiver trabalhando com classes, o ideal é utilizar uma API específica para isso
+        //quando estamos trabalhando com classes, usar isso não é o ideal, pois posso ter mais de uma classe num elemento, o ideal é utilizar uma API específica
         feedbackMessage.classList.add('show')
         feedbackMessage.getElementsByTagName('p')[0].textContent = msg
-
         feedbackMessageCloseBtn.focus()
 
         function hideErrorMessage() {
             feedbackMessage.classList.remove('show')
-            console.log('clicou close')
-            
-            
             feedbackMessageCloseBtn.removeEventListener('click', hideErrorMessage)
             feedbackMessageCloseBtn.removeEventListener('keyup', pressedKeyboardOnBtn)
-            
-            
+
             if (cb === 'function') {
                 cb()
             }
-
         };
 
         function pressedKeyboardOnBtn(e) {
@@ -60,17 +55,11 @@
             }
         }
 
-
-
         feedbackMessageCloseBtn.addEventListener('click', hideErrorMessage)
         feedbackMessageCloseBtn.addEventListener('keyup', pressedKeyboardOnBtn)
 
     }
 
-    const txtDescricao = document.getElementById('txtDescricao')
-    const contadorContainer = document.getElementById('contador')
-    const resta = contadorContainer.getElementsByTagName('span')[0]
-    const maxima = txtDescricao.maxLength
     mostrarNumero(maxima)
 
     contadorContainer.removeAttribute('style')

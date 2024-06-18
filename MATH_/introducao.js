8d8bde237aa919abeae408a39f4fd4ca97ff0232
Kaio <silv4kaio@hotmail.com>
2024-06-18 17:59:38 -0300

    objeto Math e seus métodos

---
+++ MATH_/introducao.js
@@ -0,0 +1,29 @@
+// O objeto Math NÃO é uma função construtora
+
+// min e max
+
+console.log(Math.min(1,2,3,4,5,6)) //1 
+console.log(Math.max(1,2,3,4,5,6,25)) // 25
+
+//.round() - vai arredondar o número
+// decimal abaixo de 5, arredonda pra baixo 
+console.log(Math.round(45.5)) // 46
+console.log(Math.round(45.49)) // 45
+
+// há métodos que arrendam para cima ou para baixo
+console.log(Math.floor(71.99)) // 71
+console.log(Math.ceil(71.99)) // 72
+
+// expoente
+
+console.log(Math.pow(2,4))
+// há uma maneira mais recente
+console.log(2**4)
+
+
+// raiz quadrada e cubica
+console.log(Math.sqrt(49))
+
+console.log(Math.cbrt(49)) //cubica
+
+

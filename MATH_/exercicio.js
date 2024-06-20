5cd8f8150fc47fce4374daae3a0f5eac5f83cc1c
Kaio <silv4kaio@hotmail.com>
2024-06-20 17:58:58 -0300

    Added a new file bg_random.html

--- MATH_/exercicio.js
+++ MATH_/exercicio.js
@@ -1,10 +1,6 @@
 function getRandomNumber(inicio = 0, fim = 10, integer = true){
     let nInicial = Math.random() * (fim - inicio + 1) + inicio
-    if(integer === true){
-        return parseInt(nInicial)
-    }else{
-        return nInicial
-    }
+    return integer ? parseInt(nInicial) : nInicial
 }
 
-console.log(getRandomNumber(2, 3))
+console.log(getRandomNumber(2, 3, false))
